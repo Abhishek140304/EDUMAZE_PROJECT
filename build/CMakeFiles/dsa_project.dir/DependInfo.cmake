5c277f631c7279bd5eb9b3426209669d0a997cc3
Barala0786 <ankitkumarbarala0786@gmail.com>
2025-09-01 22:35:50 +0530

    set up done

--- build/CMakeFiles/dsa_project.dir/DependInfo.cmake
+++ build/CMakeFiles/dsa_project.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "D:/DSA_project/Edumaze_Project/main.cpp" "CMakeFiles/dsa_project.dir/main.cpp.obj" "gcc" "CMakeFiles/dsa_project.dir/main.cpp.obj.d"
+  "C:/Users/DELL/Downloads/2nd year/EDUMAZE_PROJECT/main.cpp" "CMakeFiles/dsa_project.dir/main.cpp.obj" "gcc" "CMakeFiles/dsa_project.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/DSA_project/Edumaze_Project
+# Install script for directory: C:/Users/DELL/Downloads/2nd year/EDUMAZE_PROJECT
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/DSA_project/Edumaze_Project/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/DELL/Downloads/2nd year/EDUMAZE_PROJECT/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/DSA_project/Edumaze_Project/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/DELL/Downloads/2nd year/EDUMAZE_PROJECT/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
